575bf0e9b8b1e7aeb8690f7fc6b3e182a5e39fbc
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-09-08 09:45:52 -0400

    docs: documentation rewrite

--- lochness/rpms/__init__.py
+++ lochness/rpms/__init__.py
@@ -23,9 +23,9 @@ logger = logging.getLogger(__name__)
 def get_rpms_database(rpms_root_path: str) -> Dict[str, pd.DataFrame]:
     '''Return dictionary of RPMS database in pandas dataframes
 
-    Base on the date in the file name, the most recent csv file among the csv
-    files exported by the RPMS is loaded as a dictionary to be backed stored.
-    Old csv files are moved to a temporary directory.
+    Based on the date in the file name, the most recent csv file exported by
+    the RPMS is loaded as a dictionary to be stored to the phoenix directory.
+    Other old csv files are moved to a temporary directory.
 
     Key arguments:
         rpms_root_path: root of the RPMS sync directory, str.
