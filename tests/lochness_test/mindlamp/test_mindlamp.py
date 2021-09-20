ad4a12206330cdeb976253f209dfe4d977f5865c
kcho <kevincho@bwh.harvard.edu>
2021-09-20 11:27:01 -0400

    test: test clean up

--- tests/lochness_test/mindlamp/test_mindlamp.py
+++ tests/lochness_test/mindlamp/test_mindlamp.py
@@ -1,4 +1,5 @@
 import lochness.mindlamp
+from lochness.keyring import pretty_print_dict
 from lochness import config
 import os
 from pathlib import Path
@@ -43,7 +44,6 @@ class KeyringAndEncryptMindlamp(KeyringAndEncrypt):
         token = Tokens()
         mindlamp_token, access_key, secret_key, api_url = \
                 token.read_token_or_get_input('mindlamp')
-        # mindlamp_token, access_key, secret_key, api_url = \
                 # token.get_mindlamp_token()
 
         self.keyring['mindlamp.StudyA'] = {}
@@ -54,44 +54,87 @@ class KeyringAndEncryptMindlamp(KeyringAndEncrypt):
         self.write_keyring_and_encrypt()
 
 
-def test_lamp_modules():
+@pytest.fixture
+def get_lamp():
     token = Tokens()
     mindlamp_token, access_key, secret_key, api_url = \
             token.read_token_or_get_input('mindlamp')
     LAMP.connect(access_key, secret_key)
-    print(LAMP.Study.all_by_researcher('me')['data'])
-    # study_id, study_name = get_study_lamp(LAMP)
+
+    return LAMP
+
+
+def test_lamp_download_speed(get_lamp):
+    LAMP = get_lamp
+
+    subject_id = 'U6198539134'  # Yoon's data
+
+    # activity data
+    start = time.time()
+    _ = LAMP.ActivityEvent.all_by_participant(
+            subject_id)
+    end = time.time()
+    time_to_pull_all_data = end - start
+
+    start = time.time()
+    _ = LAMP.ActivityEvent.all_by_participant(
+            subject_id, _from="1626806920000", to="1627578306405")
+    end = time.time()
+    time_to_pull_partial_data = end - start
+
+    assert time_to_pull_all_data > time_to_pull_partial_data, \
+            'It took more time to pull partial activity data than ' \
+            'the full data'
+
+    # sensor data
+    start = time.time()
+    _ = LAMP.SensorEvent.all_by_participant(
+            subject_id)
+    end = time.time()
+    time_to_pull_all_data = end - start
+
+    start = time.time()
+    _ = LAMP.SensorEvent.all_by_participant(
+            subject_id, _from="1626806920000", to="1627578306405")
+    end = time.time()
+    time_to_pull_partial_data = end - start
+
+    assert time_to_pull_all_data > time_to_pull_partial_data, \
+            'It took more time to pull partial sensor data than ' \
+            'the full data'
+
+
+def test_lamp_get_studyname(get_lamp):
+    LAMP = get_lamp
     study_id, study_name = get_study_lamp(LAMP)
+    print(study_id, study_name)
+
     subject_ids = get_participants_lamp(LAMP, study_id)
+    print(subject_ids)
+
+
+def test_lamp_example_data(get_lamp):
+    LAMP = get_lamp
+    subject_ids = ['U4518928219', 'U6198539134']
 
     for subject_id in subject_ids:
-        # if subject_id == 'U7045332804':
-        if subject_id == 'U4518928219' or subject_id == 'U6198539134':
-            print(subject_id)
-            # activity_dicts = get_activities_lamp(LAMP, subject_id)
-            activity_dicts = get_activity_events_lamp(LAMP, subject_id)
-            # activity_events_dicts = LAMP.ActivityEvent.all_by_participant( subject_id, label_from = "1626806920000", to=1627578306405)
-            # activity_events_dicts = LAMP.ActivityEvent.all_by_participant(
-                    # subject_id, _from=None, to=None)
-            print(activity_dicts)
-            # print(activity_events_dicts)
-            # sys.exit()
+        activity_dicts = get_activity_events_lamp(LAMP, subject_id)
+        sensor_dicts = get_sensor_events_lamp(LAMP, subject_id)
 
-            break
-            sensor_dicts = get_sensor_events_lamp(LAMP, subject_id)
-            # print(activity_dicts)
-            # print(sensor_dicts)
+        with open(f'{subject_id}_activity_data.json', 'w') as f:
+            json.dump(activity_dicts, f)
 
-            with open(f'{subject_id}_activity_data.json', 'w') as f:
-                json.dump(activity_dicts, f)
+        with open(f'{subject_id}_sensor_data.json', 'w') as f:
+            json.dump(sensor_dicts, f)
 
-            with open(f'{subject_id}_sensor_data.json', 'w') as f:
-                json.dump(sensor_dicts, f)
-            # # break
+        with open(f'{subject_id}_activity_data.json', 'r') as f:
+            pretty_print_dict(json.load(f))
 
-    # print(os.popen('tree').read())
-    # os.remove('activity_data.json')
-    # os.remove('seonsor_data.json')
+        with open(f'{subject_id}_sensor_data.json', 'r') as f:
+            pretty_print_dict(json.load(f))
+
+        os.remove(f'{subject_id}_sensor_data.json')
+        os.remove(f'{subject_id}_activity_data.json')
 
 
 def test_sync_mindlamp(args):
@@ -118,55 +161,19 @@ def test_sync_mindlamp(args):
     show_tree_then_delete('tmp_lochness')
 
 
-def test_do_with_mindlamp(args):
-    syncArgs = SyncArgs(args.outdir)
-    sources = ['mindlamp']
-    syncArgs.update_source(sources)
+def test_sound_download(get_lamp):
+    LAMP = get_lamp
 
-    create_lochness_template(args)
-    syncArgs.config = args.outdir / 'config.yml'
-    _ = KeyringAndEncryptMindlamp(args.outdir)
+    subject_id = 'U6198539134'  # Yoon's data
 
+    activity_events_dicts = LAMP.ActivityEvent.all_by_participant(
+            subject_id, _from="1631730650831", to="1631730650833")
 
-    phoenix_root = args.outdir / 'PHOENIX'
-    information_to_add_to_metadata = {'mindlamp': {'subject_id': '1001',
-                                                   'source_id': 'U7045332804'}}
-    initialize_metadata_test(phoenix_root, 'StudyA',
-                             information_to_add_to_metadata)
-    
-    syncArgs.input_sources = syncArgs.source
-    do(syncArgs)
-    show_tree_then_delete('tmp_lochness')
+    audio = activity_events_dicts['data'][0]['static_data']['url']
+    decode_bytes = base64.b64decode(audio.split(',')[1])
 
-
-
-def test_sound_download(args):
-    token = Tokens()
-    mindlamp_token, access_key, secret_key, api_url = \
-            token.read_token_or_get_input('mindlamp')
-    LAMP.connect(access_key, secret_key)
-    print(LAMP.Study.all_by_researcher('me')['data'])
-    # study_id, study_name = get_study_lamp(LAMP)
-    study_id, study_name = get_study_lamp(LAMP)
-    subject_ids = get_participants_lamp(LAMP, study_id)
-
-    for subject_id in subject_ids:
-        if subject_id == 'U6198539134':
-            activity_events_dicts = LAMP.ActivityEvent.all_by_participant(
-                    subject_id,
-                    _from="1626806920000",
-                    to=1627578306405)
-
-            print(activity_events_dicts['data'][0])
-            print()
-            print()
-            print(activity_events_dicts['data'][1])
-
-            import base64
-            audio = activity_events_dicts['data'][0]['static_data']['url']
-            decode_bytes = base64.b64decode(audio.split(',')[1])
-            with open('prac2.mp3', 'wb') as f:
-                f.write(decode_bytes)
+    with open('test.mp3', 'wb') as f:
+        f.write(decode_bytes)
 
 
 def test_timestamp_to_UTC_0_24():
@@ -174,8 +181,6 @@ def test_timestamp_to_UTC_0_24():
     print(timestamp1)
     print(datetime.utcfromtimestamp(timestamp1))
 
-    # naive vs aware
-    
     # current time in UTC
     ct_utc = datetime.now(pytz.timezone('UTC'))
     ct_utc_00 = ct_utc.replace(hour=0, minute=0, second=0, microsecond=0)
@@ -185,28 +190,6 @@ def test_timestamp_to_UTC_0_24():
     ct_utc_24_ts = time.mktime(ct_utc_24.timetuple()) * 1000
 
     print(ct_utc_24.strftime('%Y_%m_%d'))
-    return
-
-
-    token = Tokens()
-    mindlamp_token, access_key, secret_key, api_url = \
-            token.read_token_or_get_input('mindlamp')
-    LAMP.connect(access_key, secret_key)
-    print(LAMP.Study.all_by_researcher('me')['data'])
-    # study_id, study_name = get_study_lamp(LAMP)
-    study_id, study_name = get_study_lamp(LAMP)
-    subject_ids = get_participants_lamp(LAMP, study_id)
-
-    for subject_id in subject_ids:
-        if subject_id == 'U6198539134':
-            activity_events_dicts = LAMP.ActivityEvent.all_by_participant(
-                    subject_id,
-                    _from=ct_utc_00_ts,
-                    to=ct_utc_24_ts)
-            # activity_events_dicts = LAMP.ActivityEvent.all_by_participant(
-                    # subject_id)
-            print(activity_events_dicts)
-
 
 
 @pytest.fixture
@@ -226,9 +209,7 @@ def args_and_Lochness():
     return args, lochness_obj
 
 
-# def test_initialize_metadata_then_sync(args_and_Lochness):
 def test_get_days_to_pull(args_and_Lochness):
-    print()
     args, Lochness = args_and_Lochness
     Lochness['mindlamp_days_to_pull'] = None
     days_to_pull = get_days_to_pull(Lochness)
@@ -249,7 +230,6 @@ def test_sync_mindlamp(args):
     syncArgs.config = args.outdir / 'config.yml'
     _ = KeyringAndEncryptMindlamp(args.outdir)
 
-
     phoenix_root = args.outdir / 'PHOENIX'
     information_to_add_to_metadata = {'mindlamp': {'subject_id': '1001',
                                                    'source_id': 'U6198539134'}}
@@ -260,4 +240,6 @@ def test_sync_mindlamp(args):
     for subject in lochness.read_phoenix_metadata(Lochness, syncArgs.studies):
         sync(Lochness, subject, False)
 
-    # show_tree_then_delete('tmp_lochness')
+    show_tree_then_delete('tmp_lochness')
+
+
