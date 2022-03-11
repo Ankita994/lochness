ecb2af9eb4d5ec9f4ec84dadc18e4d5ae51e677f
kcho <kevincho@bwh.harvard.edu>
2022-03-12 03:23:41 +1100

    feat: send_email_update.py to also send out check_source

--- scripts/send_email_update.py
+++ scripts/send_email_update.py
@@ -6,6 +6,7 @@ import argparse as ap
 import lochness.config as config
 from lochness.email import send_out_daily_updates
 from lochness.transfer import create_s3_transfer_table
+from lochness.utils.source_check import check_source
 from typing import List
 
 def parse_args(argv):
@@ -59,6 +60,7 @@ def send_email_update(config_file: str, days: int, recreate_db: bool,
     # update recipients
     if recipients:
         Lochness['notify'] = {'_': recipients}
+        Lochness['file_check_notify'] = {'_': recipients}
 
 
     if log_file:
@@ -76,6 +78,7 @@ def send_email_update(config_file: str, days: int, recreate_db: bool,
         create_s3_transfer_table(Lochness)
 
     send_out_daily_updates(Lochness, days, test)
+    check_source(Lochness)
 
 
 if __name__ == '__main__':
