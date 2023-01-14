70507f76ba00fcfe2f8e3a8bc39c960243251191
kcho <kevincho@bwh.harvard.edu>
2023-01-15 00:34:56 +1100

    tmp

--- lochness/utils/source_check.py
+++ lochness/utils/source_check.py
@@ -607,7 +607,7 @@ def check_source(Lochness: 'lochness', test: bool = False) -> None:
 if __name__ == '__main__':
     # testing purposes
     config_loc = '/mnt/prescient/Prescient_data_sync/config.yml'
-    config_loc = '/mnt/prescient/Prescient_production/config.yml'
+    # config_loc = '/mnt/prescient/Prescient_production/config.yml'
     # config_loc = '/opt/software/Pronet_data_sync/config.yml'
     Lochness = load(config_loc)
     Lochness['file_check_notify']['__global__'] = [
